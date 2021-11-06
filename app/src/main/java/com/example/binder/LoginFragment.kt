feb225ededca39322173afc41965d7cf1b91416d
Julia Rheingold <julia.rheingold@gmail.com>
2021-11-06 22:05:58 +0100

    Altered fragment layouts added text on change listener

--- app/src/main/java/com/example/binder/LoginFragment.kt
+++ app/src/main/java/com/example/binder/LoginFragment.kt
@@ -1,24 +1,33 @@
 package com.example.binder
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
 import com.example.binder.databinding.FragmentLoginBinding
+import com.example.binder.viewmodel.LoginViewModel
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class LoginFragment : Fragment() {
 
+    private var _viewModel: LoginViewModel? = null
     private var _binding: FragmentLoginBinding? = null
-
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -26,12 +35,30 @@ class LoginFragment : Fragment() {
     ): View? {
 
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
-        return binding.root
+        _viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
 
+        _binding?.editLogin?.addTextChangedListener(object: TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                _viewModel?.login = s.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+
+            }
+        })
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         binding.buttonLogin.setOnClickListener {
             findNavController().navigate(R.id.action_Login_to_SecondFragment)
---
+++ app/src/main/java/com/example/binder/viewmodel/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.binder.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    var login: String = ""
+    var password: String = ""
+}
+
